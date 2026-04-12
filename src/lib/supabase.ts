96091044d525ae92cc859362d773c4c6a5ec72b6
Zamad Shakeel <zamadshakil@gmail.com>
2026-04-13 01:38:30 +0500

    chore: remove hardcoded supabase connect string, utilize secure environment variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // We fallback to hardcoded strings as these are public ANONYMOUS keys and purely for telemetry telemetry.
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://wslxfwkxwbnrjyemetja.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzbHhmd2t4d2Jucmp5ZW1ldGphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNTM1NDYsImV4cCI6MjA4ODkyOTU0Nn0.fTXHhxSBVfEAFw_Bix5JczVQrJCkE5S26I54L-NRfL4";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
